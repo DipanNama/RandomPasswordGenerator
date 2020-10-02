fa5f3e36393c3b78aa1aab5f4ca366fd04b40779
Ruan Aragão <ruanaragao2@gmail.com>
2020-10-02 13:58:12 -0300

    Add static files

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,21 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+      <meta property="og:title" content="" />
+      <meta property="og:type" content="" />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
+
+      <link rel="manifest" href="site.webmanifest" />
+      <link rel="apple-touch-icon" href="icon.png" />
+
+      <link rel="stylesheet" href="/css/normalize.css" />
+      <link rel="stylesheet" href="/css/main.css" />
+
+      <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Rokkitt:wght@300&display=swap" rel="stylesheet" />
+
+      <meta name="theme-color" content="#fafafa" />
     </Head>
     <header>
       <nav>
