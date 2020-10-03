6e7c17e7518225acc4142acc12782df0ef11ac4e
Ruan Aragão <ruanaragao2@gmail.com>
2020-10-03 12:36:13 -0300

    Password generate functional

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,43 @@
-import Layout from '../components/Layout'
-import Generate from '../components/Generate'
+import React, { useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+import Generate from '../components/Generate';
 
-function handlerGenerate (length = 10 as number) {
-  // Generate
-  const password = Generate(length)
-  return password
+function IndexPage () {
+  // The password
+  const [pass, setPass] = useState<String>('');
+
+  // Call function Generate and persist result
+  function generatePassword (length = 10 as number) {
+    const password = Generate(length)
+    setPass(password)
+  }
+
+  // Generate password on load page
+  useEffect(() => {
+    generatePassword()
+  }, [])
+
+  // Generate passowrd on button press
+  function handlerGenerate (length = 10 as number) {
+    generatePassword(length)
+  }
+
+  const Password = (): JSX.Element => {
+    return <>{pass}</>;
+  }
+
+  return (
+    <Layout title="Random Password Generator">
+      <main className="main-content">
+        <div className="password-wrapper">
+          <span className="password">
+            <Password />
+          </span>
+          <button onClick={() => handlerGenerate(12)} className="btn-generate">GENERATE PASSWORD</button>
+        </div>
+      </main>
+    </Layout>
+  )
 }
-
-const Password = (): JSX.Element => {
-  return <>{handlerGenerate()}</>;
-}
-
-const IndexPage = () => (
-  <Layout title="Random Password Generator">
-    <main className="main-content">
-      <div className="password-wrapper">
-        <span className="password">
-          <Password />
-        </span>
-        <button onClick={() => handlerGenerate(12)} className="btn-generate">GENERATE PASSWORD</button>
-      </div>
-    </main>
-  </Layout>
-)
 
 export default IndexPage
