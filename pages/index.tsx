157575c32ebf486b3da0fd0b07adf8cd3754a78a
Ruan Aragão <ruanaragao2@gmail.com>
2020-10-04 23:50:33 -0300

    Layout responsive buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,8 +43,10 @@ function IndexPage () {
           <span className="password">
             <Password />
           </span>
-          <button onClick={() => handlerGenerate(option)} className="btn-generate">GENERATE PASSWORD</button>
-          <button onClick={() => Copy2Clipboard(pass)} className="btn-copy">COPY</button>
+          <div className="buttons-wrapper">
+            <button onClick={() => handlerGenerate(option)} className="btn-generate">GENERATE PASSWORD</button>
+            <button onClick={() => Copy2Clipboard(pass)} className="btn-copy">COPY</button>
+          </div>
           <div className="option-wrapper">
             <form action="">
               <div className="option-password">
